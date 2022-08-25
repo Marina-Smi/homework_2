dd5782673640a9bfd064494123d3b917a7ca65dd
Marina90 <miss.honeymari@gmail.com>
2022-08-25 20:42:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+//задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+              Console.WriteLine("{0}->{1}",num);
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+while(true)
+    	if(num > 99)
+	{
+  	  Console.WriteLine(( num / 100) % 10);
+	}
+        else
+            Console.WriteLine("no");
+
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+Console.WriteLine("Введите номер дня недели':");
+  int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 6)
+	{
+  	  Console.WriteLine("Рабочий день");
+	}
+else
+    {
+     Console.WriteLine("Выходной");
+    }
+
+*/
+
+
+
